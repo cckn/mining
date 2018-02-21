79072158267dd77ccdbe0482c7c9ca33bed9dde1
DS <cckn1985@gmail.com>
2018-02-21 23:20:49 +0900

    음.. 밴 목록 갱신 시도 중 .. 문제가 생겼다.

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -2,7 +2,8 @@ const models = require('../models.js');
 
 module.exports = () => {
     const options = {
-        force: process.env.NODE_ENV === 'test' ? true : false,
+        // force: process.env.NODE_ENV === 'test' ? true : false,
+        force: true,
     };
     return models.sequelize.sync(options);
 };
