d58340fc19dcc5bb75505cf55692e74fcc03e7ec
DS <cckn1985@gmail.com>
2018-03-18 21:34:53 +0900

    responseModel 데이터 수집 구조 변경

--- src/main.js
+++ src/main.js
@@ -1,33 +1,12 @@
 'use strict';
 
-const axios = require('axios');
 const app = require('../app');
-
-const api_config = require('./config/api');
-
 const responseModel = require('./models/responseModel');
 
-const getMatchId = () => {
-    return 3040204762;
-};
 let matchId = 3040204762;
 
-const main = async (matchId) => {
-    try {
-        const res = await axios.get(
-            `https://kr.api.riotgames.com/lol/match/v3/matches/${matchId}?api_key=${
-                api_config.key
-            }`
-        );
-        console.log(res.headers);
-    } catch (error) {
-        console.log(error.response);
-    }
 
-    // const data = new responseModel(res);
-};
 
-setInterval(() => {
-    main(matchId);
-    matchId++;
-}, 200);
+const resModel = new responseModel(matchId);
+
+resModel.getResponse()
--- src/models/responseModel.js
+++ src/models/responseModel.js
@@ -1,19 +1,44 @@
+'use strict';
+
+
+const axios = require('axios');
 const matchModel = require('./matchModel');
+const api_config = require('../config/api.json');
 
 class Response {
-    constructor(response) {
+    constructor(matchId) {
+        this.matchId = matchId;
+        console.log(`constructor :: ${this.matchId}`);
+    }
+
+    async getResponse() {
+        const url = `https://kr.api.riotgames.com/lol/match/v3/matches/${
+            this.matchId
+        }?api_key=${api_config.key}`;
+
+        try {
+            const res = await axios.get(url);
+
+            this.getData(res)
+        } catch (error) {
+            console.log(`catch :: ${error.response.status}`);
+        }
+    }
+
+
+    getData(response) {
         const res = response;
 
         // console.log(response);
 
         this.status = res.status;
         this.headers = res.headers;
-        
-        
+
         this.match = new matchModel(res.data);
 
-        // this.show();
+        this.show();
     }
+
     show() {
         console.log(this);
     }
